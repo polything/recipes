1ef4d2064f779613d3bf4fc7b1a9cf5b23a268fe
Akira Heid <mheid2011@gmail.com>
2018-08-24 17:13:56 -0400

    Update README with configuration how-to

--- js/localDB.js
+++ js/localDB.js
@@ -1,7 +1,7 @@
 const fs = require('fs')
 const config = require('./config')
 
-const dbFile = __dirname + '/../dbAdapters/' + config.options.dbAdapter.type + '.js'
+const dbFile = __dirname + '/../dbAdapters/' + config.options.localDB.type + '.js'
 // Import the db adapter based on config file
 var db = null
 fs.stat(dbFile, (err, _) => {
