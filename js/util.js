6d00662bc0c50db0ed641bf833c2b1e5c696ea4a
akira <akiraheid@users.noreply.github.com>
2019-12-29 10:30:41 -0500

    Standardize recipe add/edit pages
    Use the same form for adding and editing recipes to make the recipe
    editing interaction consistent.

--- js/util.js
+++ js/util.js
@@ -5,11 +5,18 @@ exports.send200 = (res, data={}) => {
 	res.status(200).json(data).end()
 }
 
-// Publish 400 response with message.
+// Publish 400 response with data.
+//
+// If `data` is a String, the string will be assigned to an object. Otherwise,
+// `data` is sent.
 // @param res{Object} The response object.
-// @param msg{String} Message to send.
-exports.send400 = (res, msg) => {
-	res.status(400).json({msg: msg}).end()
+// @param data{String or Object} Message to send.
+exports.send400 = (res, data) => {
+	if (typeof(data) === 'string') {
+		data = { msg: data }
+	}
+
+	res.status(400).json(data).end()
 }
 
 // Publish 500 response with message
